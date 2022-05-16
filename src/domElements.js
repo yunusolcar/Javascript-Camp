0cac2b1489a0a2eb285e8df6d04cf32ea4f7427b
yunus <yunusolcar@hotmail.com>
2022-05-16 11:59:14 +0300

    js camp day 9

---
+++ src/domElements.js
@@ -0,0 +1,19 @@
+/* Single/Multiple Elements
+
+document.all -> kod genelinde bütün aramalar gerçekleştirilir.
+örn: document.all[50] -> 50. indisdeki elemanı getirir.
+
+document.querySelector(".h3") -> class ı h3 olan elemanı getirir
+document.getElementById("title") -> id si title olaman elemanı getirir
+document.getElementById("title").className -> id si title olan elamanın classı yazılır
+document.getElementById("title").classList -> id si title olan elemanı liste olarak yazar
+document.getElementById("title").style.fontSize ="30px"; -> title ın font büyüklüğünü 30 px olarak değiştirebiliriz
+document.querySelector("#title") ->id si title olan ilk elemanı getirir
+document.querySelector(".cartTitle") -> classı cartTitle olan elemanı getirir.
+document.querySelector("li") -> ilk "li" elemanını getirir
+document.getElementsByClassName("task")[1] -> class ismi task olanları getirir
+
+
+
+
+*/
