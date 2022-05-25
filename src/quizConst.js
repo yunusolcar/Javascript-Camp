019074a80d1af160870bac60f11a1b328ddf63b4
yunus <yunusolcar@hotmail.com>
2022-05-25 10:30:00 +0300

    js camp day 18

--- src/quizConst.js
+++ src/quizConst.js
@@ -1,75 +1,90 @@
+// OOP: Nesne Tabanlı Programlama
+
 function Question(questionText, answerOptions, correctAnswer) {
     this.questionText = questionText;
     this.answerOptions = answerOptions;
     this.correctAnswer = correctAnswer;
 }
 
-Question.prototype.checkAnswer = function (answer) {
+Question.prototype.checAnswer = function (answer) {
     return answer === this.correctAnswer
 }
 
-let Question1 = new Question("Hangisi bir JS paket yönetim uygulamasıdır?", { a: "Node.js", b: "Typescript", c: "Npm" }, "c");
-let Question2 = new Question("Hangisi bir .net paket yönetim uygulamasıdır?", { a: "Node.js", b: "nuget", c: "Npm" }, "b");
-
-let allQuestion = [
-    new Question("1-Hangisi bir JS paket yönetim uygulamasıdır?", { A: ": Node.js", B: ": Typescript", C: ": Npm", D: ": Nuget" }, "c"),
-    new Question("2-Hangisi bir tS paket yönetim uygulamasıdır?", { a: ": Node.js", b: ": Typescript", c: ": Npm" }, "c"),
-    new Question("3-Hangisi bir lS paket yönetim uygulamasıdır?", { a: ": Node.js", b: ": Typescript", c: ": Npm" }, "c"),
-    new Question("4-Hangisi bir kS paket yönetim uygulamasıdır?", { a: ": Node.js", b: ": Typescript", c: ": Npm" }, "c")
+let allQuestions = [
+    new Question("1-Hangisi javascript paket yönetim uygulasıdır?", { a: "Node.js", b: "Typescript", c: "Npm", d: "Nuget" }, "c"),
+    new Question("2-Hangisi javascript paket yönetim uygulasıdır?", { a: "Node.js", b: "Typescript", c: "Npm" }, "c"),
+    new Question("3-Hangisi javascript paket yönetim uygulasıdır?", { a: "Node.js", b: "Typescript", c: "Npm" }, "c"),
+    new Question("4-Hangisi javascript paket yönetim uygulasıdır?", { a: "Node.js", b: "Typescript", c: "Npm" }, "c")
 ];
 
-//Quiz Exam Questions
-function Quiz(allQuestion) {
-    this.allQuestion = allQuestion;
+function Quiz(allQuestions) {
+    this.allQuestions = allQuestions;
     this.questionIndex = 0;
 }
 
 Quiz.prototype.getQuestion = function () {
-    return this.allQuestion[this.questionIndex]
+    return this.allQuestions[this.questionIndex];
 }
 
-const quiz = new Quiz(allQuestion);
+const quiz = new Quiz(allQuestions);
 
 document.querySelector(".btn_start").addEventListener("click", function () {
-    document.querySelector(".quiz_box").classList.add("active")
-    showQuestion(quiz.getQuestion())
-}
-)
-
-document.querySelector(".next_btn").addEventListener("click", function () {
-    if (quiz.allQuestion.length != quiz.questionIndex + 1) { //dizi içerisindeki eleman sayısına eşit olana kadar devam edip sonunda durur
-        quiz.questionIndex += 1;
-        showQuestion(quiz.getQuestion())
-    } else {
-        console.log("Quiz over!");
-    }
+    document.querySelector(".quiz_box").classList.add("active");
+    showQuestion(quiz.getQuestion());
+    document.querySelector(".next_btn").classList.remove("show");
 })
 
-function showQuestion(Question) {
-    let questionT = `<span>${Question.questionText}</span>`;
+document.querySelector(".next_btn").addEventListener("click", function () {
+    if (quiz.allQuestions.length != quiz.questionIndex + 1) {
+        quiz.questionIndex += 1;
+        showQuestion(quiz.getQuestion());
+        document.querySelector(".next_btn").classList.remove("show");
+    } else {
+        console.log("Quiz Over!");
+    }
+});
+
+const option_list = document.querySelector(".option_list");
+const correctIcon = '<div class="icon"><i class="fas fa-check"></i></div>';
+const incorrectIcon = '<div class="icon"><i class="fas fa-times"></i></div>';
+
+function showQuestion(soru) {
+    let question = `<span>${soru.questionText}</span>`;
     let options = '';
 
-    for (let choice in Question.answerOptions) {
+    for (let answer in soru.answerOptions) {
         options +=
             `
-                <div class="option">
-                    <span>${choice} ${Question.answerOptions[choice]}</span>
+                <div class="option"> 
+                    <span><b>${answer}</b>: ${soru.answerOptions[answer]}</span>
                 </div>
             `;
     }
-    const option_list = document.querySelector(".option_list");
-    document.querySelector(".question_text").innerHTML = questionT;
+    document.querySelector(".question_text").innerHTML = question;
     option_list.innerHTML = options;
 
-    const optionAll = option_list.querySelectorAll(".option");
-    console.log(optionAll);
+    const option = option_list.querySelectorAll(".option");
 
-    for (let opt of optionAll) {
+    for (let opt of option) {
         opt.setAttribute("onclick", "optionSelected(this)")
     }
-
 }
+
 function optionSelected(option) {
-    let choice = option.querySelector("span b").textContent;
-    console.log(choice);
+    let answer = option.querySelector("span b").textContent;
+    let soru = quiz.getQuestion();
+
+    if (soru.checAnswer(answer)) {
+        option.classList.add("correct");
+        option.insertAdjacentHTML("beforeend", correctIcon);
+    } else {
+        option.classList.add("incorrect");
+        option.insertAdjacentHTML("beforeend", incorrectIcon);
+    }
+
+    for (let i = 0; i < option_list.children.length; i++) {
+        option_list.children[i].classList.add("disabled");
+    }
+
+    document.querySelector(".next_btn").classList.add("show");
 }
